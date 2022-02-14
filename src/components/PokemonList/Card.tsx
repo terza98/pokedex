68ce46d39be399f7f5b52261bd2d0b4edefaf354
terza98 <terza98@hotmail.rs>
2022-02-14 12:08:40 +0100

    add dummy data and some simple styles

--- src/components/PokemonList/Card.tsx
+++ src/components/PokemonList/Card.tsx
@@ -1,11 +1,58 @@
-import { Box, BoxProps, useColorModeValue } from "@chakra-ui/react";
+import {
+  BoxProps,
+  Flex,
+  Box,
+  Image,
+  Text,
+  useColorModeValue,
+} from "@chakra-ui/react";
+import Link from "next/link";
+
+const traitStyles = {
+  p: "2px 10px",
+  borderRadius: 5,
+};
 
 export const Card = (props: BoxProps) => (
-  <Box
+  <Flex
     minH="3xs"
     bg="bg-surface"
+    alignItems="center"
+    justify="center"
+    flexDir="column"
     boxShadow={useColorModeValue("sm", "sm-dark")}
     borderRadius="lg"
+    border="1px solid"
+    borderColor="grey"
     {...props}
-  />
+  >
+    <Link href="#">
+      <Image
+        cursor="pointer"
+        src="https://assets.pokemon.com/assets/cms2/img/pokedex/detail/001.png"
+      />
+    </Link>
+    <Flex
+      flexDir="column"
+      alignItems="flex-start"
+      w="100%"
+      maxW="150px"
+      mb="10px"
+    >
+      <Text color="grey" fontSize={12} fontWeight="bold">
+        #303
+      </Text>
+      <Text color="black" fontSize={20}>
+        Bulbasaur
+      </Text>
+      <Flex gap="5px">
+        <Box bg="green" {...traitStyles}>
+          <Text color="white">Grass</Text>
+        </Box>
+        <Box bg="purple" {...traitStyles}>
+          <Text color="white">Poison</Text>
+        </Box>
+      </Flex>
+    </Flex>
+  </Flex>
 );
