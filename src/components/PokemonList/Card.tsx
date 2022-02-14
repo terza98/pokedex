a0269e5823c8952327335a12ec2ee30f8584eddd
terza98 <terza98@hotmail.rs>
2022-02-14 12:17:03 +0100

    add favorite button and hover action

--- src/components/PokemonList/Card.tsx
+++ src/components/PokemonList/Card.tsx
@@ -5,54 +5,78 @@ import {
   Image,
   Text,
   useColorModeValue,
+  IconButton,
 } from "@chakra-ui/react";
 import Link from "next/link";
+import { useState } from "react";
+import { FiHeart } from "react-icons/fi";
 
 const traitStyles = {
   p: "2px 10px",
   borderRadius: 5,
 };
 
-export const Card = (props: BoxProps) => (
-  <Flex
-    minH="3xs"
-    bg="bg-surface"
-    alignItems="center"
-    justify="center"
-    flexDir="column"
-    boxShadow={useColorModeValue("sm", "sm-dark")}
-    borderRadius="lg"
-    border="1px solid"
-    borderColor="grey"
-    {...props}
-  >
-    <Link href="#">
-      <Image
-        cursor="pointer"
-        src="https://assets.pokemon.com/assets/cms2/img/pokedex/detail/001.png"
-      />
-    </Link>
+export const Card = (props: BoxProps) => {
+  const [isHovered, setIsHovered] = useState<boolean>(false);
+
+  return (
     <Flex
+      minH="3xs"
+      bg="bg-surface"
+      alignItems="center"
+      justify="center"
       flexDir="column"
-      alignItems="flex-start"
-      w="100%"
-      maxW="150px"
-      mb="10px"
+      position="relative"
+      boxShadow={useColorModeValue("sm", "sm-dark")}
+      borderRadius="lg"
+      border="1px solid"
+      borderColor="grey"
+      {...props}
     >
-      <Text color="grey" fontSize={12} fontWeight="bold">
-        #303
-      </Text>
-      <Text color="black" fontSize={20}>
-        Bulbasaur
-      </Text>
-      <Flex gap="5px">
-        <Box bg="green" {...traitStyles}>
-          <Text color="white">Grass</Text>
-        </Box>
-        <Box bg="purple" {...traitStyles}>
-          <Text color="white">Poison</Text>
-        </Box>
+      <IconButton
+        aria-label="favorite"
+        position="absolute"
+        top={0}
+        right={0}
+        bg="transparent"
+        _hover={{
+          bg: "transparent",
+        }}
+        _active={{
+          bg: "transparent",
+        }}
+        onMouseEnter={() => setIsHovered(true)}
+        onMouseLeave={() => setIsHovered(false)}
+        icon={<FiHeart fill={isHovered ? "red" : "transparent"} color="red" />}
+      />
+      <Link href="#">
+        <Image
+          cursor="pointer"
+          src="https://assets.pokemon.com/assets/cms2/img/pokedex/detail/001.png"
+        />
+      </Link>
+      <Flex
+        flexDir="column"
+        alignItems="flex-start"
+        w="100%"
+        maxW="150px"
+        mb="10px"
+      >
+        <Text color="grey" fontSize={12} fontWeight="bold">
+          #303
+        </Text>
+        <Text fontSize={20} mt={2}>
+          Bulbasaur
+        </Text>
+        <Flex gap="5px">
+          <Box bg="green" {...traitStyles}>
+            <Text color="white">Grass</Text>
+          </Box>
+          <Box bg="purple" {...traitStyles}>
+            <Text color="white">Poison</Text>
+          </Box>
+        </Flex>
       </Flex>
     </Flex>
-  </Flex>
-);
+  );
+};
