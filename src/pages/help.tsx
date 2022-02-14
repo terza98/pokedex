5e5c9eb21308b9ba3f10c34b159bfaea5ff092ed
terza98 <terza98@hotmail.rs>
2022-02-14 18:44:17 +0100

    update help page

--- src/pages/help.tsx
+++ src/pages/help.tsx
@@ -1,9 +1,29 @@
+import { Flex, Text } from "@chakra-ui/react";
+import Link from "next/link";
 import { BaseLayout } from "../components/BaseLayout";
+import { Container } from "../components/Container";
 
 const Index = () => (
   <BaseLayout>
-    If you have any problems or concerns about the website feel free to contact
-    me @: mladen@mladenterzic.com
+    <Container w="100%">
+      <Flex
+        alignItems="center"
+        justify="center"
+        flexDir="column"
+        w="100%"
+        h="100%"
+      >
+        <Text px={10}>
+          If you have any problems or concerns about the website feel free to
+          contact me @:{" "}
+          <Link href="mailto:mladen@mladenterzic.com">
+            <Text as="span" color="blue">
+              mladen@mladenterzic.com
+            </Text>
+          </Link>
+        </Text>
+      </Flex>
+    </Container>
   </BaseLayout>
 );
 
