e75130d47e9d66aa62eaefafe592f17f95281ef6
terza98 <terza98@hotmail.rs>
2022-02-14 11:45:58 +0100

    clean repo

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,13 @@
-import { Flex, FlexProps } from '@chakra-ui/react'
+import { Flex, FlexProps, Link, Text } from "@chakra-ui/react";
 
 export const Footer = (props: FlexProps) => (
-  <Flex as="footer" py="8rem" {...props} />
-)
+  <Flex as="footer" py="3rem" {...props}>
+    <Text>
+      Created by{" "}
+      <Link href="https://mladenterzic.com" color="blue">
+        Mladen Terzic
+      </Link>{" "}
+      with React ❤️ Next ❤️ Typescript ❤️ Chakra
+    </Text>
+  </Flex>
+);
