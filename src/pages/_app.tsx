94869ac01bbe896dec733d712b34f5f011d87f70
terza98 <terza98@hotmail.rs>
2022-02-16 13:01:13 +0100

    add apolo client and graphql

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,10 +2,14 @@ import { ChakraProvider } from "@chakra-ui/react";
 
 import theme from "../theme";
 import { AppProps } from "next/app";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider resetCSS theme={theme}>
+      <Head>
+        <title>Pokèdex</title>
+      </Head>
       <Component {...pageProps} />
     </ChakraProvider>
   );
